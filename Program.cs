6ca97b0f41ee8807216c07d31672c6938005d5f2
Jacob Egner <jmegner@gmail.com>
2022-03-20 18:46:10 -0500

    add code, update readme

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+main();
+
+const int NumDieSides = 6;
+const int NumDiceThrownPerSide = 6;
+
+void main()
+{
+    var rng = new Random();
+    var pipCounts1 = new int[NumDieSides];
+    var pipCounts2 = new int[NumDieSides];
+    var matchCounts = new int[NumDieSides + 1];
+    var numSimulations = (int)1e8;
+
+    for (int i = 0; i < numSimulations; i++)
+    {
+        var matchCount = mirrorOfMindsMatchCount(pipCounts1, pipCounts2, rng);
+        matchCounts[matchCount]++;
+    }
+
+    Console.WriteLine("NumMatches,n(NumMatches),p(NumMatches),p(>=NumMatches),p(<=NumMatches)");
+
+    int runningSum = 0;
+    double expectedValue = 0;
+
+    for(int damage = 0; damage < matchCounts.Length; damage++)
+    {
+        var matchCount = matchCounts[damage];
+        var frequency = matchCount / (double)numSimulations;
+        expectedValue += damage * frequency;
+
+        Console.WriteLine(
+            damage
+            + ", " + matchCount.ToString("D9")
+            + ", " + frequency.ToString("N4")
+            + ", " + ((numSimulations - runningSum) / (double)numSimulations).ToString("N4")
+            + ", " + ((matchCount + runningSum) / (double)numSimulations).ToString("N4")
+            );
+        runningSum += matchCount;
+    }
+
+    Console.WriteLine("expectedValue=" + expectedValue.ToString("N4"));
+}
+
+int mirrorOfMindsMatchCount(int[] pipCounts1, int[] pipCounts2, Random rng)
+{
+    var matchCount = 0;
+    fillPipCounts(pipCounts1, rng);
+    fillPipCounts(pipCounts2, rng);
+
+    for (int pipIdx = 0; pipIdx < pipCounts1.Length; pipIdx++)
+    {
+        matchCount += Math.Min(pipCounts1[pipIdx], pipCounts2[pipIdx]);
+    }
+
+    return matchCount;
+}
+
+void fillPipCounts(int[] pipCounts, Random rng)
+{
+    Array.Clear(pipCounts);
+
+    for (int i = 0; i < NumDiceThrownPerSide; i++)
+    {
+        pipCounts[rng.Next(6)]++;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # KillTeamMirrorOfMinds
-Monte Carlo simulation to estimate damage probabilities for Mirror Of Minds psychic power.
+
+This is a Monte Carlo simulation to estimate damage/match probabilities for
+Mirror Of Minds psychic power.
+
+## Background
+
+Kill Team (2021 edition) has a Harlequin Void-Dance Trouple kill team (revealed
+in White Dwarf 474).  The Void-Dancer Troupe Shadowseer operative can perform a
+Mirror Of Minds psychic power:
+
+> Select one enemy operative within this operative's Line of Sight.  Both
+players roll six D6.  Pair your dice with your opponent's dice based on
+matching results.  For each matching pair, that enemy operative suffers 1
+mortal wound.  For example, if you roll ***6***, ***5***, 5, ***4***, 2, ***1*** and
+your opponent rolls ***6***, ***5***, ***4***, 4, 3, ***1***, that enemy operative
+would suffer 4 mortal wounds.
+
+## Output
+```
+NumMatches,n(NumMatches),p(NumMatches),p(>=NumMatches),p(<=NumMatches)
+0, 000610928, 0.0061, 1.0000, 0.0061
+1, 005817186, 0.0582, 0.9939, 0.0643
+2, 021348422, 0.2135, 0.9357, 0.2778
+3, 036445107, 0.3645, 0.7222, 0.6422
+4, 027794217, 0.2779, 0.3578, 0.9202
+5, 007577990, 0.0758, 0.0798, 0.9959
+6, 000406150, 0.0041, 0.0041, 1.0000
+expectedValue=3.0935
+```
